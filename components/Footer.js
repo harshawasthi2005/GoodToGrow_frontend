bc13ea4c706b916c2eb14e1f1f4fb1bcff557401
harshawasthi2005 <harshawasthi265@gmail.com>
2023-09-20 11:56:51 +0530

    latest update

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
           <a href="https://twitter.com"><i className="fab fa-twitter fa-2x" style={{"color": "#55acee"}}></i></a>
           <a href="https://www.facebook.com"><i className="fab fa-facebook-f fa-2x" style={{"color": '#3b5998'}}></i></a>
           <a href="https://www.instagram.com"><i className="fab fa-instagram fa-2x" style={{"color": "#ac2bac"}}></i></a>
-          <a href="https://www.github.com"><i class="fab fa-github fa-2x" style={{"color": "#ffff"}}></i></a>
+          <a href="https://www.github.com"><i className="fab fa-github fa-2x" style={{"color": "#ffff"}}></i></a>
         </div>
       </div>
       <p className={styles.copy}>&copy; 2023 E-Learning Website. All Rights Reserved.</p>
